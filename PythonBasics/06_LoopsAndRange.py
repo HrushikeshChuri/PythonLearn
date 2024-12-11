adb07caefcd2ef7c87da489c775e89d416697e8c
HrushikeshChuri <67365737+HrushikeshChuri@users.noreply.github.com>
2024-12-11 19:50:41 +0530

    changes v7

--- PythonBasics/06_LoopsAndRange.py
+++ PythonBasics/06_LoopsAndRange.py
@@ -14,6 +14,20 @@ while counter < 10:
     counter += 1
 
 #  2. For Loop
-
+# Note that second argument number is excluded from range generation.
+# range(0,10) will only generate numbers from 0 till 9
+ 
 for counter in range(0,10):
     print(counter)
+
+
+# 3.a Break
+
+counter = 0
+
+while True:
+    if counter > 9:
+        break
+    print(counter)
+    
+# 4.b Continue
