ee34cfcbc556b9786ba41e47815d0f9234fd709c
HrushikeshChuri <67365737+HrushikeshChuri@users.noreply.github.com>
2024-12-11 18:11:17 +0530

    changes v6

---
+++ PythonBasics/06_LoopsAndRange.py
@@ -0,0 +1,19 @@
+#######################################
+# Loops and Range
+# 1. While Loop
+# 2. For Loop
+# 3. Break and continue
+#######################################
+
+#   1. While Loop
+
+counter = 0
+
+while counter < 10:
+    print(counter)
+    counter += 1
+
+#  2. For Loop
+
+for counter in range(0,10):
+    print(counter)
---
+++ PythonBasics/07_ListAndTuples.py
@@ -0,0 +1,9 @@
+#######################################
+# List and Tuples
+#######################################
+
+#   list with different data types
+#   Accessing list items
+#   Looping and list comprehention  
+#   Add/remove/change list items
+#   other list methods (len/count, sort/reverse, copy/append/extend, clear/pop/remove, index/insert)
